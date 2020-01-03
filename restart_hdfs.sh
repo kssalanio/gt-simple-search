edc131c01e222c9667a755a61d6aeddf6b6324cf
Ken Salanio <kssalanio@gmail.com>
2020-01-03 08:11:38 +0800

    split restart cluster script to hdfs and spark

---
+++ restart_hdfs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Restart HDFS
+stop-dfs.sh
+start-dfs.sh
--- restart_spark.sh
+++ restart_spark.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Restart HDFS
-stop-dfs.sh
-start-dfs.sh
-
 # Restart Spark daemons
 /home/ubuntu/spark/sbin/stop-all.sh
 /home/ubuntu/spark/sbin/start-all.sh
